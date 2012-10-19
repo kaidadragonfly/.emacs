7b65847d2c2bdd6e8e1b3c3eaf105ec72b1effd7
Kaida <kaidadragonfly@gmail.com>
2012-10-19 15:31:48 -0700

    Rename

--- emacsrc.el
+++ emacsrc.el
@@ -1,12 +1,12 @@
-;; Load custom elisp files.  
+;; Load custom elisp files.
 (add-to-list 'load-path "~/.emacs.d/lib")
 
 ;;----------------------------------------------------------------------------
-;; Interface related items.  
+;; Interface related items.
 ;;----------------------------------------------------------------------------
 ;; Make file completion case insensitive.
 (setq read-file-name-completion-ignore-case t)
-;; Display buffers interactively when switching.  
+;; Display buffers interactively when switching.
 (iswitchb-mode t)
 (iswitchb-default-keybindings)
 ;; Rectangles :)
@@ -16,29 +16,29 @@
 (global-set-key "\M-'" 'iedit-mode)
 ;; Make line numbers have a space after them.
 (setq linum-format "%3d ")
-;; Highlight parenthesis.  
+;; Highlight parenthesis.
 (show-paren-mode 1)
 ;; Get rid of the initially useful, but ultimately annoying splash screen.
 (setq inhibit-startup-message t)
 ;; Give buffers unique names.
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
-;; Remove menu bar in text-mode.  
+;; Remove menu bar in text-mode.
 (when (and (fboundp 'window-system) (not (window-system))) (menu-bar-mode 0))
 ;; Reload changed files automatically.
 (global-auto-revert-mode)
-;; Set up winner mode.  
+;; Set up winner mode.
 (winner-mode t)
-;; Set up meta-arrows to move around the windows.  
+;; Set up meta-arrows to move around the windows.
 (global-set-key (kbd "M-<up>")    'windmove-up)
 (global-set-key (kbd "M-<down>")  'windmove-down)
 (global-set-key (kbd "M-<left>")  'windmove-left)
 (global-set-key (kbd "M-<right>") 'windmove-right)
-;; Save all backup files in one directory.  
+;; Save all backup files in one directory.
 (if (file-directory-p "~/.emacs.d/backups")
     (setq backup-directory-alist '(("." . "~/.emacs.d/backups"))))
-;; Set compilation window height.  
-(setq compilation-window-height 0)      ;Hide the window.  
+;; Set compilation window height.
+(setq compilation-window-height 0)      ;Hide the window.
 ;; Silence flyspell welcome message.
 (setq flyspell-issue-welcome-flag nil)
 
@@ -46,7 +46,7 @@
 ;; Sometimes needed for usage inside of tmux.
 (global-set-key (kbd "<select>") 'move-end-of-line)
 
-;; Load modular settings.  
+;; Load modular settings.
 (require 'elisp-load-dir)
 (elisp-load-dir "~/.emacs.d/load")
 
@@ -67,9 +67,9 @@ Kills the old scratch buffer.  "
     (lisp-interaction-mode)))
 
 ;;-----------------------------------------------------------------------------
-;; Content related items.  
+;; Content related items.
 ;;-----------------------------------------------------------------------------
-;; Require a newline at the end of the file.  
+;; Require a newline at the end of the file.
 (setq require-final-newline t)
 ;; Don't use tabs for indentation.
 (setq-default indent-tabs-mode nil)
