30199a9d2123a6db77449d9d29bdbff0af91c169
Kaida <kaidadragonfly@gmail.com>
2013-02-25 09:33:44 -0800

    Use pylint instead of pyflakes

--- load/py.el
+++ load/py.el
@@ -12,17 +12,25 @@
                                       (interactive)
                                       (back-to-indentation)
                                       (python-backspace 1)))
-   ;; Setup flymake
+   ;; Four spaces, no tabs, don't guess!
+   (setq tab-width 4
+         indent-tabs-mode nil
+         python-guess-indent nil)
+   ;;
+   ;; Setup flymake.
+   ;;
+   ;; Use pylint.
    (when (load "flymake" t)
-     (defun flymake-pyflakes-init ()
+     (defun flymake-pylint-init ()
        (let* ((temp-file (flymake-init-create-temp-buffer-copy
                           'flymake-create-temp-inplace))
               (local-file (file-relative-name
                            temp-file
                            (file-name-directory
                             buffer-file-name))))
-         (list "pyflakes" (list local-file))))
-     
+         (list "epylint" (list local-file))))
+
      (add-to-list 'flymake-allowed-file-name-masks
-                  '("\\.py\\'" flymake-pyflakes-init)))
+                  '("\\.py\\'" flymake-pylint-init)))
+   ;; Start flymake on file load.
    (add-hook 'find-file-hook 'flymake-find-file-hook)))
