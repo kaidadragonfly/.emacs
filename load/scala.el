56dd2c64313517990d5322b92ee59011d4ee643c
Kaida <kaidadragonfly@gmail.com>
2013-11-04 09:41:23 -0800

    Update iedit, disable scala.el
    scala.el pulls in some things incompatible with the old version of Emacs
    bundled with 12.04.

--- load/scala.el
+++ load/scala.el
@@ -1,12 +1,12 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa"
-               . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-(unless (package-installed-p 'scala-mode2)
-  (package-refresh-contents) (package-install 'scala-mode2))
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa"
+;;                . "http://melpa.milkbox.net/packages/") t)
+;; (package-initialize)
+;; (unless (package-installed-p 'scala-mode2)
+;;   (package-refresh-contents) (package-install 'scala-mode2))
 
-(add-hook
- 'scala-mode-hook
- (lambda ()
-   (run-hooks 'c-mode-common-hook)))
+;; (add-hook
+;;  'scala-mode-hook
+;;  (lambda ()
+;;    (run-hooks 'c-mode-common-hook)))
