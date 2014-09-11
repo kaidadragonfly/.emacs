91a9526ef2899dffb462996525dac6e74d854615
Kaida <kaidadragonfly@gmail.com>
2014-09-11 08:47:05 -0700

    Bind iedit-global to M-" and local to M-'

--- load/iedit.el
+++ load/iedit.el
@@ -2,8 +2,8 @@
 (add-to-list 'load-path "~/.emacs.d/lib/iedit")
 (require 'iedit)
 
-(defun iedit-dwim (&optional arg)
-  "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+(defun iedit-global (&optional arg)
+  "Starts iedit but uses \\[narrow-to-region] to limit its scope."
   (interactive "P")
   (if arg
       (iedit-mode)
@@ -13,8 +13,26 @@
             (iedit-done)
           (iedit-mode))))))
 
+(defun iedit-local (&optional arg)
+  "Starts iedit but uses \\[narrow-to-defun] to limit its scope."
+  (interactive "P")
+  (if arg
+      (iedit-mode)
+    (save-excursion
+      (save-restriction
+        (widen)
+        ;; Shrink scope.
+        (narrow-to-defun)
+
+        (if iedit-mode
+            (iedit-done)
+          (iedit-mode))))))
+
 ;; Bind to M-'.
-(global-set-key "\M-'" 'iedit-dwim)
+(global-set-key "\M-'" 'iedit-local)
+
+;; Bind to M-"
+(global-set-key "\M-\"" 'iedit-global)
 
 ;; Make C-g disable iedit-mode.
 (defadvice keyboard-quit (before keyboard-quit-iedit activate)
