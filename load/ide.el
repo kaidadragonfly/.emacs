ef6e58f1f3e43f239df9b7233768f849067d9de1
Kaida <kaidadragonfly@gmail.com>
2014-09-12 22:06:52 -0700

    Various tweaks to fix bugs in smart-tab

--- load/ide.el
+++ load/ide.el
@@ -5,8 +5,8 @@
 (defun smart-beginning-of-line ()
   "Move point to first non-whitespace character or beginning-of-line.
 
-Move point to the first non-whitespace character on this line.
-If point was already at that position, move point to beginning of line."
+   Move point to the first non-whitespace character on this line.
+   If point was already at that position, move point to beginning of line."
   (interactive)
   (let ((oldpos (point)))
     (back-to-indentation)
@@ -24,7 +24,7 @@ If point was already at that position, move point to beginning of line."
 ;; Smart indent region.
 (defun smart-indent-region ()
   "If the mark is active, indents region.
-    Otherwise it indents the entire file."
+   Otherwise it indents the entire file."
   (interactive)
   (if mark-active
       (indent-region (region-beginning)
@@ -37,21 +37,26 @@ If point was already at that position, move point to beginning of line."
 
 (defun long-enough-p ()
   "Is this word long enough to expand?"
-  (> (- (point) (car (bounds-of-thing-at-point 'word))) 2))
+  (let ((old-point (point)))
+    (save-excursion (> (- old-point (point)) 2))))
+
 (defun override-p ()
   "Did we manually override the length limit?"
   (eq this-command last-command))
 
+(defvar smart-tab-always-indent nil)
+(setq-local smart-tab-always-indent nil)
+
 ;; Modified from http://emacswiki.org/emacs/TabCompletion
 (defun smart-tab ()
   "Smart tab does the following:
-    In the mini-buffer: expand the word.
+   In the mini-buffer: expand the word.
 
-    If mark is active: indent the region.
+   If mark is active: indent the region.
 
-    If we are at the end of the word or call twice: expand the word.
+   If we are at the end of the word or call twice: expand the word.
 
-    Otherwise: indent the current line."
+   Otherwise: indent the current line."
   (interactive)
   ;; Always expend in the buffer.
   (if (minibufferp)
@@ -62,18 +67,22 @@ If point was already at that position, move point to beginning of line."
                        (region-end))
       ;; Are we at the end of a word?
       (if (looking-at "\\_>")
-          (if (long-enough-p)
-              (dabbrev-expand nil)
-            (if (override-p)
-                (progn
-                  (dabbrev--reset-global-variables)
-                  (dabbrev-expand nil))
-              nil))
+          (progn
+            (if smart-tab-always-indent
+                (indent-for-tab-command) nil)
+            (if (long-enough-p)
+                (dabbrev-expand nil)
+              (if (override-p)
+                  (progn
+                    (dabbrev--reset-global-variables)
+                    (dabbrev-expand nil)
+                    (indent-for-tab-command)))))
         (indent-for-tab-command)))))
 ;; Make dabbrev-replace case insensitive.
+;; insensitive.
 (setq dabbrev-case-replace nil)
 ;; Replace dabbrev-expand with hippie-expand.
-;; (global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "M-/") 'hippie-expand)
 ;; Bind to tab.
 (global-set-key (kbd "C-i") 'smart-tab)
 ;; Function to enable autosave.
@@ -83,24 +92,8 @@ If point was already at that position, move point to beginning of line."
   (interactive)
   (if (and (buffer-file-name) (buffer-modified-p))
       (save-some-buffers t)))
-;; Add an auto save hook to enable.
-;; (add-hook 'auto-save-hook 'save-buffer-if-visiting-file)
-
-;; Make yank indent.
-;; Modified from: http://emacswiki.org/emacs/AutoIndentation
-;; (setq yank-no-indent-modes '())
-
-;; Disabled because it has been simpler/safer to
-;; indent region after a yank.
-;; (dolist (command '(yank yank-pop))
-;;   (eval `(defadvice ,command (after indent-region activate)
-;;            (not current-prefix-arg)
-;;            (let
-;;                ((mark-even-if-inactive transient-mark-mode))
-;;              (if (not (member major-mode yank-no-indent-modes))
-;;                  (indent-region
-;;                   (region-beginning) (region-end) nil))))))
 
 ;; Rectangles :)
 (cua-selection-mode t)
-(global-set-key "\C-j" 'cua-set-rectangle-mark)
+;; (kbd "C-j") is Ctrl + Enter
+(global-set-key (kbd "C-j") 'cua-set-rectangle-mark)
