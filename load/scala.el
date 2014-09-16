c2273afe22a8a474360089b512f6f6c046b0158e
Kaida <kaidadragonfly@gmail.com>
2014-09-16 14:49:41 -0700

    Wrap scala-el in ignore-errors

--- load/scala.el
+++ load/scala.el
@@ -1,12 +1,16 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa"
-               . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-(unless (package-installed-p 'scala-mode2)
-  (package-refresh-contents) (package-install 'scala-mode2))
+;; The following requires emacs 24.
+;; Using ignore-errors so that the rest of the config loads on earlier
+;; versions.
+(ignore-errors
+  (require 'package)
+  (add-to-list 'package-archives
+               '("melpa"
+                 . "http://melpa.milkbox.net/packages/") t)
+  (package-initialize)
+  (unless (package-installed-p 'scala-mode2)
+    (package-refresh-contents) (package-install 'scala-mode2))
 
-(add-hook
- 'scala-mode-hook
- (lambda ()
-   (run-hooks 'c-mode-common-hook)))
+  (add-hook
+   'scala-mode-hook
+   (lambda ()
+     (run-hooks 'c-mode-common-hook))))
