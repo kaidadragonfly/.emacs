bf3b15ce6d5c5abf3076e15b6ce7b4e4b2409ff4
kaidadragonfly <kaidadragonfly@gmail.com>
2015-01-12 14:25:46 -0800

    Tweak flycheck-check-buffer so it modifies the file

--- load/flyhooks.el
+++ load/flyhooks.el
@@ -18,6 +18,15 @@
 
 (flycheck-add-next-checker 'sbt 'scala)
 
+(defun touch-and-check-buffer ()
+  "Touch and save the buffer before checking syntax."
+  (interactive)
+  (save-excursion
+    (end-of-line nil)
+    (insert " ")
+    (save-buffer)
+    (flycheck-buffer)))
+
 (global-set-key (kbd "<f7>") 'flycheck-previous-error)
-(global-set-key (kbd "<f8>") 'flycheck-buffer)
+(global-set-key (kbd "<f8>") 'touch-and-check-buffer)
 (global-set-key (kbd "<f9>") 'flycheck-next-error)
