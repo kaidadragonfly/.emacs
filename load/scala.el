2668b07e4dfb3d5c59bd73b071ec5974755cbff9
kaidadragonfly <kaidadragonfly@gmail.com>
2015-03-05 13:37:01 -0800

    Improve flycheck scala config

--- load/scala.el
+++ load/scala.el
@@ -1,45 +1,30 @@
-(defun test ()
-  "Open the test for the current file."
-  (interactive)
-  (let ((base (replace-regexp-in-string
-               "/src/main/" "/src/test/" buffer-file-name)))
-    (find-file (replace-regexp-in-string "[.]scala" "Test.scala" base))))
-
-(defun main ()
-  "Open the main source for the current file."
-  (interactive)
-  (let ((base (replace-regexp-in-string
-               "/src/test/" "/src/main/" buffer-file-name)))
-    (find-file (replace-regexp-in-string "Test[.]scala" ".scala" base))))
-
-;; The following requires emacs 24.
-;; Using ignore-errors so that the rest of the config loads on earlier
-;; versions.
-(ignore-errors
-  (add-hook 'scala-mode-hook
-            (lambda ()
-              (run-hooks 'c-mode-common-hook)
-              (flyspell-prog-mode)
-              (auto-fill-mode 0)
-              (set-fill-column 80)
-              (auto-revert-mode t)
-              (local-set-key (kbd "RET") 'newline-and-indent)
-              ;; Use tags.
-              (require 'etags)
-              ;; Only check syntax on load and save
-              (defvar flycheck-check-syntax-automatically)
-              (setq-local flycheck-check-syntax-automatically
-                          '(mode-enabled save))
-              ;; Show margin.
-              (require 'fill-column-indicator)
-              (declare-function fci-mode
-                                "fill-column-indicator.el"
-                                nil)
-              (defvar fci-rule-character-color)
-              (setq-local fci-rule-character-color "color-234")
-              (if (> (window-width) (current-fill-column))
-                  (progn (fci-mode)
-                         (toggle-truncate-lines nil))))))
+(add-hook 'scala-mode-hook
+          (lambda ()
+            (run-hooks 'c-mode-common-hook)
+            (flyspell-prog-mode)
+            (auto-fill-mode 0)
+            (set-fill-column 80)
+            (auto-revert-mode t)
+            (local-set-key (kbd "RET") 'newline-and-indent)
+            ;; Use tags.
+            (require 'etags)
+            ;; Only check syntax on load and save
+            (defvar flycheck-check-syntax-automatically)
+            (setq-local flycheck-check-syntax-automatically
+                        '(mode-enabled save))
+            ;; Disable "scala" checker.
+            (defvar flycheck-disabled-checkers "flycheck.el")
+            (setq flycheck-disabled-checkers (cons 'scala flycheck-disabled-checkers))
+            ;; Show margin.
+            (require 'fill-column-indicator)
+            (declare-function fci-mode
+                              "fill-column-indicator.el"
+                              nil)
+            (defvar fci-rule-character-color)
+            (setq-local fci-rule-character-color "color-234")
+            (if (> (window-width) (current-fill-column))
+                (progn (fci-mode)
+                       (toggle-truncate-lines nil)))))
 
 ;; Define a sbt checker!
 (require 'flycheck)
@@ -47,19 +32,23 @@
   "Checker for compilation with SBT"
   :command ("esbt")
   :error-patterns
-  ((error line-start "[error] " (file-name) ":" line ": "
+  ((error line-start "[error] "
+          ;; Ignore column info for now, since scalastyle uses 0
+          ;; indexed columns, instead of one indexed, and it's
+          ;; non-trivial to work around.
+          (file-name) ":" line ":" (optional (one-or-more digit) ":") " "
           (message (zero-or-more not-newline)
-                   (one-or-more "\n" blank
-                                (zero-or-more not-newline)))
+                   (zero-or-more "\n" blank (zero-or-more not-newline)))
           line-end)
-   (warn line-start "[warn] " (file-name) ":" line ": "
+   (warn line-start "[warn] "
+         (file-name) ":" line ":" (optional (one-or-more digit) ":") " "
          (message (zero-or-more not-newline)
-                  (one-or-more "\n" blank
-                               (zero-or-more not-newline)))
+                  (zero-or-more "\n" blank (zero-or-more not-newline)))
          line-end))
   :modes scala-mode)
 
 (flycheck-add-next-checker 'sbt 'scala)
+
 
 ;; Rebuild tags on save.
 (declare-function rebuild-tags "ide.el")
