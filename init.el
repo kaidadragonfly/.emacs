53777ab169f6b50e3c7e43a982f9d3917c1e19d6
kaidadragonfly <kaidadragonfly@gmail.com>
2015-04-07 10:54:11 -0700

    Override case-fold-search as something changed it

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Load custom elisp files.
 (add-to-list 'load-path "~/.emacs.d/lib")
-;; Load legacy compatability.  
+;; Load legacy compatability.
 (load "~/.emacs.d/lib/legacy-compat.el")
 ;; Initialize packages.
 (load "~/.emacs.d/install-packages")
@@ -93,6 +93,7 @@ Kills the old scratch buffer.  "
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(case-fold-search t)
  '(flycheck-checkers
    (quote
     (sbt ada-gnat asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint d-dmd elixir emacs-lisp emacs-lisp-checkdoc erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck haml handlebars haskell-ghc haskell-hlint html-tidy javascript-jshint javascript-eslint javascript-gjslint json-jsonlint less lua make perl perl-perlcritic php php-phpmd php-phpcs puppet-parser puppet-lint python-flake8 python-pylint racket rpm-rpmlint rst rst-sphinx ruby-rubocop ruby-rubylint ruby ruby-jruby rust sass scala scala-scalastyle scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim tex-chktex tex-lacheck texinfo verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby)))
