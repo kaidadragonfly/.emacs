d4dfaad7d6d3725f3e76e19246f372a479efd880
kaidadragonfly <kaidadragonfly@gmail.com>
2015-05-27 09:35:53 -0700

    Ignore page-up (shift-space hack)

--- load/osx.el
+++ load/osx.el
@@ -6,12 +6,13 @@
 
       (global-set-key (kbd "<mouse-4>") '(lambda ()
                                            (interactive)
-                                           (scroll-down
-                                            1)))
+                                           (scroll-down 1)))
       (global-set-key (kbd "<mouse-5>") '(lambda ()
                                            (interactive)
-                                           (scroll-up
-                                            1)))
+                                           (scroll-up 1)))
+      (global-set-key (kbd "<prior>") '(lambda ()
+                                         (interactive)
+                                         (insert " ")))
       (declare-function cua-copy-region (arg) "cua-base.el")
       (defun smart-copy-region ()
         (interactive)
