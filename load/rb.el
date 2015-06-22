98d072a567f24197523de238b68a313d4d9ab6e7
kaidadragonfly <kaidadragonfly@gmail.com>
2015-06-22 13:01:29 -0700

    Add whitespace hook

--- load/rb.el
+++ load/rb.el
@@ -6,4 +6,6 @@
    ;; Turn on auto-fill-mode
    (auto-fill-mode 1)
    ;; Make enter indent.
-   (local-set-key (kbd "C-m") 'newline-and-indent)))
+   (local-set-key (kbd "C-m") 'newline-and-indent)
+   ;; Clean up whitespace on save.
+   (add-hook 'before-save-hook 'whitespace-cleanup)))
