03e0c5e85a91e433bef09a7cf42902918b82fe0b
kaidadragonfly <kaidadragonfly@gmail.com>
2016-06-15 17:00:06 -0700

    Disable foodcritic

--- load/rb.el
+++ load/rb.el
@@ -8,4 +8,7 @@
    ;; Make enter indent.
    (local-set-key (kbd "C-m") 'newline-and-indent)
    ;; Clean up whitespace on save.
-   (add-hook 'before-save-hook 'whitespace-cleanup)))
+   (add-hook 'before-save-hook 'whitespace-cleanup)
+   (defvar flycheck-disabled-checkers "flycheck.el")
+   (setq flycheck-disabled-checkers
+         (cons 'chef-foodcritic flycheck-disabled-checkers))))
