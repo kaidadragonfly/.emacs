abd829e34cb0451aa7ae852987e166c074f17db6
kaidadragonfly <kaidadragonfly@gmail.com>
2016-08-27 19:45:20 -0700

    Add rust config

---
+++ load/rs.el
@@ -0,0 +1,23 @@
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+(require 'flycheck)
+(flycheck-add-next-checker 'rust-cargo 'rust)
+
+(add-hook
+ 'rust-mode-hook
+ (lambda ()
+   ;; Activate auto-fill-mode.
+   (auto-fill-mode 1)
+   ;; Allow movement between subwords.
+   (subword-mode 1)
+   (let ((entry (assq 'subword-mode minor-mode-alist)))
+     (when entry (setcdr entry '(nil))))
+   ;; Activate flyspell-prog-mode.
+   (flyspell-prog-mode)
+   ;; Make newline automatically indent the next line.
+   (local-set-key (kbd "C-m") 'reindent-then-newline-and-indent)
+   ;; Clean whitespace on save.
+   (add-hook 'before-save-hook 'whitespace-cleanup)
+   ;; Make smart-tab always indent.
+   (defvar smart-tab-always-indent)
+   (set (make-local-variable 'smart-tab-always-indent) t)))
