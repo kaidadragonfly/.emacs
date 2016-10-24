a9570f461b0fcb5a8345b05ddc344f137bcc962f
kaidadragonfly <kaidadragonfly@gmail.com>
2016-10-24 10:27:55 -0700

    Rebuild tags in ruby-mode

--- load/rb.el
+++ load/rb.el
@@ -9,6 +9,9 @@
    (local-set-key (kbd "C-m") 'newline-and-indent)
    ;; Clean up whitespace on save.
    (add-hook 'before-save-hook 'whitespace-cleanup)
+   ;; Rebuild tags on save.
+   (add-hook (make-local-variable 'after-save-hook) 'rebuild-tags)
+   ;; Disable foodcritic, because our chef files are sad.
    (defvar flycheck-disabled-checkers "flycheck.el")
    (setq flycheck-disabled-checkers
          (cons 'chef-foodcritic flycheck-disabled-checkers))))
