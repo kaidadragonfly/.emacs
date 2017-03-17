73cefcacfbab38f6be001c133799fecf9042de47
kaidadragonfly <kaidadragonfly@gmail.com>
2017-03-17 10:55:01 -0700

    Compile alchemist in a separate directory

--- load/ex.el
+++ load/ex.el
@@ -1,6 +1,11 @@
+(eval-when-compile (require 'cl))
+
 (add-hook
  'elixir-mode-hook
  (lambda ()
+   ;; Compile alchemist in a different directory.
+   (make-local-variable 'process-environment)
+   (pushnew "ALCHEMIST_MODE=1" process-environment)
    (alchemist-mode t)
    (require 'flycheck-mix)
    (flycheck-mix-setup)
