6b2df391f13a69061796ace329defd5e0477c034
kaidadragonfly <kaidadragonfly@gmail.com>
2018-12-13 10:46:33 -0800

    Disable projectile

--- init.el
+++ init.el
@@ -145,19 +145,10 @@ Kills the old scratch buffer.  "
  '(nanowrimo-today-goal-calculation-function (quote nanowrimo-today-goal-from-date))
  '(package-selected-packages
    (quote
-    (alchemist yaml-mode web-mode toml-mode sql-indent scss-mode sbt-mode s rust-mode projectile markdown-mode json-mode js2-mode iedit haskell-mode flycheck-rust fill-column-indicator feature-mode etags-select elixir-mode dockerfile-mode company apples-mode)))
+    (alchemist yaml-mode web-mode toml-mode sql-indent scss-mode sbt-mode s rust-mode markdown-mode json-mode js2-mode iedit haskell-mode flycheck-rust fill-column-indicator feature-mode etags-select elixir-mode dockerfile-mode company apples-mode)))
  '(paren-face-modes
    (quote
     (lisp-mode emacs-lisp-mode lisp-interaction-mode ielm-mode scheme-mode inferior-scheme-mode clojure-mode cider-repl-mode nrepl-mode arc-mode inferior-arc-mode elixir-mode ruby-mode)))
- '(projectile-mode t nil (projectile))
- '(projectile-mode-line
-   (quote
-    (:eval
-     (if
-         (file-remote-p default-directory)
-         " Projectile"
-       (format " [%s]"
-               (projectile-project-name))))))
  '(ruby-deep-indent-paren nil)
  '(scala-indent:align-parameters t)
  '(scala-indent:default-run-on-strategy 1)
