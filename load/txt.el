06db0f971d5cb717ff4f854e6f330c376871c2f9
kaidadragonfly <kaidadragonfly@gmail.com>
2019-08-01 13:09:28 -0700

    Add # comments to text-mode

--- load/txt.el
+++ load/txt.el
@@ -6,11 +6,15 @@
    (flyspell-mode 1)
    ;; Setup hard line wrapping.
    (auto-fill-mode 1)
-   ;; Setup tabs/indentation.  
+   ;; Disable flycheck.
+   (flycheck-mode 0)
+   ;; Setup tabs/indentation.
    (set (make-local-variable 'tab-width) 4)
    (make-local-variable 'tab-stop-list)
    (setq tab-stop-list (let ((stops '(4)))
-                      (while (< (car stops)
-                                120)
-                        (setq stops (cons (+ 4 (car stops)) stops)))
-                      (nreverse stops)))))
+                         (while (< (car stops)
+                                   120)
+                           (setq stops (cons (+ 4 (car stops)) stops)))
+                         (nreverse stops)))
+   (setq-local comment-start "#")
+   (font-lock-add-keywords nil '(("#.+" . font-lock-comment-face)))))
