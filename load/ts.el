b50fe150399208acedd89af750448d9b87f8ad4a
kaidadragonfly <kaidadragonfly@gmail.com>
2019-08-06 11:42:22 -0700

    Disable eslint for typescript files

--- load/ts.el
+++ load/ts.el
@@ -1,19 +1,23 @@
 (add-hook
  'typescript-mode-hook
  (lambda ()
-  ;; Activate auto-fill-mode.
-  (set-fill-column 100)
-  (auto-fill-mode t)
-  ;; Enable Flyspell.
-  (flyspell-prog-mode)
-  ;; Make enter indent.
-  (local-set-key (kbd "RET") 'newline-and-indent)
-  ;; Set indent level.
-  (defvar typescript-indent-level)
-  (setq typescript-indent-level 2)
-  ;; Clean whitespace on save.
-  (add-hook 'before-save-hook 'whitespace-cleanup nil t)
-  ;; Allow movement between subwords.
-  (subword-mode 1)
-  (require 'diminish)
-  (diminish 'subword-mode)))
+   ;; Activate auto-fill-mode.
+   (set-fill-column 100)
+   (auto-fill-mode t)
+   ;; Enable Flyspell.
+   (flyspell-prog-mode)
+   ;; Disable eslint
+   (defvar flycheck-disabled-checkers)
+   (setq-local flycheck-disabled-checkers
+               (cons 'javascript-eslint flycheck-disabled-checkers))
+   ;; Make enter indent.
+   (local-set-key (kbd "RET") 'newline-and-indent)
+   ;; Set indent level.
+   (defvar typescript-indent-level)
+   (setq typescript-indent-level 2)
+   ;; Clean whitespace on save.
+   (add-hook 'before-save-hook 'whitespace-cleanup nil t)
+   ;; Allow movement between subwords.
+   (subword-mode 1)
+   (require 'diminish)
+   (diminish 'subword-mode)))
