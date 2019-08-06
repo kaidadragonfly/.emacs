d999ffbba823727e0e5fc195a5510c9d43253e26
kaidadragonfly <kaidadragonfly@gmail.com>
2019-08-06 10:45:07 -0700

    Set typescript indent level to 2

--- load/ts.el
+++ load/ts.el
@@ -8,6 +8,9 @@
   (flyspell-prog-mode)
   ;; Make enter indent.
   (local-set-key (kbd "RET") 'newline-and-indent)
+  ;; Set indent level.
+  (defvar typescript-indent-level)
+  (setq typescript-indent-level 2)
   ;; Clean whitespace on save.
   (add-hook 'before-save-hook 'whitespace-cleanup nil t)
   ;; Allow movement between subwords.
