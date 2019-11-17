c3d662940fd137e50193c2260302f44d1f87ea5f
kaidadragonfly <kaidadragonfly@gmail.com>
2019-11-17 14:36:06 -0800

    Add back js2-mode-hook

--- load/js.el
+++ load/js.el
@@ -2,24 +2,23 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
-(defun js-hook-fun ()
-  ;; Activate auto-fill-mode.
-  (set-fill-column 100)
-  (auto-fill-mode t)
-  ;; Enable Flyspell.
-  (flyspell-prog-mode)
-  ;; Make enter indent.
-  (local-set-key (kbd "RET") 'newline-and-indent)
-  ;; Clean whitespace on save.
-  (add-hook 'before-save-hook 'whitespace-cleanup nil t)
-  ;; Rebuild tags on save.
-  (add-hook (make-local-variable 'after-save-hook) 'rebuild-tags nil t)
-  ;; Allow movement between subwords.
-  (subword-mode 1)
-  (require 'diminish)
-  (diminish 'subword-mode))
-
 (add-hook
  'js2-mode-hook
  (lambda ()
-   (local-set-key (kbd "<f8>") 'js2-next-error)))
+   ;; Activate auto-fill-mode.
+   (set-fill-column 100)
+   (auto-fill-mode t)
+   ;; Enable Flyspell.
+   (flyspell-prog-mode)
+   ;; Make enter indent.
+   (local-set-key (kbd "RET") 'newline-and-indent)
+   ;; Clean whitespace on save.
+   (add-hook 'before-save-hook 'whitespace-cleanup nil t)
+   ;; Rebuild tags on save.
+   (add-hook (make-local-variable 'after-save-hook) 'rebuild-tags nil t)
+   ;; Make f8 move to the next error.
+   (local-set-key (kbd "<f8>") 'js2-next-error)
+   ;; Allow movement between subwords.
+   (subword-mode 1)
+   (require 'diminish)
+   (diminish 'subword-mode)))
