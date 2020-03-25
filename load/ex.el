baef83d26fa8c3516f29219216964b79c156fcee
kaidadragonfly <kaidadragonfly@gmail.com>
2020-03-24 19:25:07 -0700

    Make elixir reindent on newline

--- load/ex.el
+++ load/ex.el
@@ -15,9 +15,8 @@
    ;; (defvar alchemist-mode-map)
    ;; (define-key alchemist-mode-map (kbd "M-.") 'xref-find-definitions)
    
-   ;; Bind C-m (newline) to `electric-indent-just-newline`
    (defvar elixir-mode-map)
-   (define-key elixir-mode-map (kbd "C-m") 'newline-and-indent)
+   (define-key elixir-mode-map (kbd "C-m") 'reindent-then-newline-and-indent)
    ;; Clean up whitespace on save.
    (add-hook 'before-save-hook 'whitespace-cleanup nil t)
    ;; Rebuild tags after save.
