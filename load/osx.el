93499f7dcf85f193c30a577c4e49dafce626e575
Kaida <kaidadragonfly@gmail.com>
2022-10-31 10:07:10 -0700

    Remove debug logging, switch to using "when"

--- load/osx.el
+++ load/osx.el
@@ -1,29 +1,23 @@
 ;; mouse integration
 (require 'mwheel)
 
-(if (equal (getenv "TERM_PROGRAM") "iTerm.app")
-    (progn
-      (require 'mouse) ;; needed for iterm2 compatibility
-      (message
-       "********************************************************************************")
-      (message "HERE")
-      (message
-       "********************************************************************************")
-      (xterm-mouse-mode)
-      (defvar mouse-wheel-progressive-speed)
-      (setq mouse-wheel-progressive-speed nil)
-      (defvar mouse-wheel-scroll-amount)
-      (setq mouse-wheel-scroll-amount '(1))
+(when (equal (getenv "TERM_PROGRAM") "iTerm.app")
+  (require 'mouse) ;; needed for iterm2 compatibility
+  (xterm-mouse-mode)
+  (defvar mouse-wheel-progressive-speed)
+  (setq mouse-wheel-progressive-speed nil)
+  (defvar mouse-wheel-scroll-amount)
+  (setq mouse-wheel-scroll-amount '(1))
 
-      (mouse-wheel-mode 1)
+  (mouse-wheel-mode 1)
 
-      (declare-function cua-copy-region (arg) "cua-base.el")
-      (defun smart-copy-region ()
-        (interactive)
-        (cua-copy-region nil)
-        (shell-command-on-region
-         (region-beginning)
-         (region-end)
-         "pbcopy")
-        (message ""))
-      (global-set-key (kbd "M-w") 'smart-copy-region)))
+  (declare-function cua-copy-region (arg) "cua-base.el")
+  (defun smart-copy-region ()
+    (interactive)
+    (cua-copy-region nil)
+    (shell-command-on-region
+     (region-beginning)
+     (region-end)
+     "pbcopy")
+    (message ""))
+  (global-set-key (kbd "M-w") 'smart-copy-region))
