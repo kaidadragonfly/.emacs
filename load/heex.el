e832e99cd81767b4abb75bc462f3d088c4a9ea40
Kaida <kaidadragonfly@gmail.com>
2022-12-28 15:49:51 -0800

    Create HEEX mode

--- load/heex.el
+++ load/heex.el
@@ -1,1 +1,18 @@
-(add-to-list 'auto-mode-alist '("\\.heex\\'" . web-mode))
+(define-derived-mode heex-mode web-mode
+  "HEEX"
+  "A variant of web-mode.")
+
+(add-hook
+ 'heex-mode-hook
+ (lambda ()
+   ;; Copied from ex.el
+   (defun elixir-format-quietly ()
+     (interactive)
+     (let ((inhibit-message t))
+       (elixir-format)))
+   ;; Format on save.
+   (add-hook 'before-save-hook 'elixir-format-quietly nil t)
+   ;; Instead of indenting region format file.
+   (define-key heex-mode-map (kbd "C-M-\\") 'elixir-format)))
+
+(add-to-list 'auto-mode-alist '("\\.heex\\'" . heex-mode))
