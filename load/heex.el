86fdc97c3ac0d5cee151065579e182cef99e312a
Kaida <kaidadragonfly@gmail.com>
2023-01-13 16:08:38 -0800

    Enable LSP for HEEX modes

--- load/heex.el
+++ load/heex.el
@@ -8,6 +8,13 @@
    ;; Format on save.
    (add-hook 'before-save-hook 'elixir-format-quietly nil t)
    ;; Instead of indenting region format file.
-   (define-key heex-mode-map (kbd "C-M-\\") 'elixir-format)))
+   (define-key heex-mode-map (kbd "C-M-\\") 'elixir-format)
+   ;; Enable LSP.
+   (defvar lsp-language-id-configuration)
+   (with-eval-after-load 'lsp-mode
+     (add-to-list 'lsp-language-id-configuration
+               '(heex-mode . "elixir")))
+
+   (lsp)))
 
 (add-to-list 'auto-mode-alist '("\\.heex\\'" . heex-mode))
