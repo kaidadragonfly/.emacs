2ee8efce7569e8211fe1f7657fad6672b9cfc6db
Kaida <kaidadragonfly@gmail.com>
2024-03-14 16:56:57 -0700

    Use web-mode for html

--- load/html.el
+++ load/html.el
@@ -1,9 +1,18 @@
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+
 (add-hook
- 'mhtml-mode-hook
+ 'web-mode-hook
  (lambda ()
    ;; Activate auto-fill-mode.
    (auto-fill-mode t)
    ;; Enable Flyspell.
    (flyspell-prog-mode)
+   ;; Use visual-line-mode instead of auto-fill-mode
+   (auto-fill-mode 0)
+   (visual-line-mode 1)
    ;; Add "automatic" indentation.
    (local-set-key (kbd "RET") 'reindent-then-newline-and-indent)))
+
+(require 'flycheck)
+(flycheck-add-mode 'html-tidy 'web-mode)
+(flycheck-add-mode 'css-stylelint 'web-mode)
